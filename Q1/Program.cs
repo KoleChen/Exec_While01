1ab8693049a3263b826d39ceaa47c41063dbbd7c
User <User@DESKTOP-M776RP5>
2022-10-23 20:50:31 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int i = 1;
+			int answer = 0;
+			while (true)
+			{
+				answer += i;
+				if (answer >= 105)
+					break;
+				i++;
+			}
+
+			Console.WriteLine(i);
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int start = 0;
+			int end = 300;
+			while (start <= end)
+			{
+				start++;
+				if (300 % start == 0)
+				{
+					Console.WriteLine(start);
+				}
+			}
+		}
+	}
+}
